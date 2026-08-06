9a03af98eb6ad0175820b1bd2f4fd2a6eedd75ba
lavpreetpannu6-jpg <lavpreetpannu6@gmail.com>
2026-08-06 21:23:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+// PANNU RASOI
 
+console.log("Welcome to PANNU RASOI");
+
+// Button click
+const btn = document.querySelector(".btn");
+
+if (btn) {
+    btn.addEventListener("click", function () {
+        alert("🙏 PANNU RASOI में आपका स्वागत है!\nजल्द ही ऑनलाइन ऑर्डर सुविधा शुरू होगी।");
+    });
+}
+
+// Footer year auto update
+const footer = document.querySelector("footer p");
+
+if (footer) {
+    footer.innerHTML = `© ${new Date().getFullYear()} PANNU RASOI | All Rights Reserved`;
+}
